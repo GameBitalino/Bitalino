9b69c9fde34b783fb616f8408e3514bc3ba78019
Cindy <cindy.v@seznam.cz>
2020-03-21 16:37:56 +0100

    statistics

--- results/reaction_time_statistics.py
+++ results/reaction_time_statistics.py
@@ -1,9 +1,14 @@
 from pandas import read_csv
 import numpy as np
 from pandas import DataFrame as df
-import os
+import os, csv
+from itertools import zip_longest
 
 path = "Bětka"
+
+sex = "F"  # F/M
+gamer = False
+
 entries = os.listdir(path)
 
 
@@ -27,11 +32,86 @@ def load_concrete_reaction_times(path):
     return change_to_green, change_to_red, display_ambulance
 
 
+mean_each_game = []
+
+mean_changed_green = []
+mean_changed_red = []
+mean_displayed_ambulance = []
+
+game = 0
+
 for signal in entries:
     file = signal[:-4]
     if signal[-3:] != "csv":
         pass
     elif file[-11:] == "whole_data_":
-        path = path + os.sep + signal
-        reaction_time, ambulance, start_stimulus, detected_activity = load_information(path)
-        change_to_green, change_to_red, display_ambulance = load_information(path)
+        game = game + 1
+        path_signal = path + os.sep + signal
+        reaction_time, ambulance, start_stimulus, detected_activity = load_information(path_signal)
+        change_to_green, change_to_red, display_ambulance = load_concrete_reaction_times(path_signal)
+
+        mean_each_game.append(np.mean(reaction_time[np.array(np.where(reaction_time > 0))[0]]))
+
+        # green
+        if change_to_green.shape[0] > 0:
+            gr = np.mean(change_to_green.iloc[:, 0])  # reaction times
+        else:
+            gr = None
+        mean_changed_green.append(gr)
+        # red
+        if change_to_red.shape[0] > 0:
+            red = np.mean(change_to_red.iloc[:, 0])  # reaction times
+        else:
+            red = None
+        mean_changed_red.append(red)
+        # ambulance
+        if display_ambulance.shape[0] > 0:
+            display_ambulance.append(np.mean(display_ambulance.iloc[:, 0]))
+        else:
+            display_ambulance.append(None)
+
+# save data
+game = [1, 2, 3, 4, 5]
+data = [game, mean_each_game, mean_changed_green, mean_changed_red, mean_displayed_ambulance]
+with open(path + os.sep + "results.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Game", "Mean", "Green", "Red", "Ambulance"])
+    for values in zip_longest(*data):
+        writer.writerow(values)
+
+# calculate mean throw all games
+# remove None: [i for i in a if i]
+mean_all_games = np.mean(mean_each_game)
+mean_all_games = round(mean_all_games, 2)
+mean_green = np.mean(mean_changed_green)
+mean_green = round(mean_green,2)
+
+mean_changed_red = [i for i in mean_changed_red if i]
+if len(mean_changed_red):
+    mean_red = np.mean(mean_changed_red)
+    mean_red = round(mean_red, 2)
+else:
+    mean_red = np.nan
+
+mean_displayed_ambulance = [i for i in mean_displayed_ambulance if i]
+if len(mean_displayed_ambulance) > 0:
+    mean_ambulance = np.mean(mean_displayed_ambulance)
+    mean_ambulance = round(mean_ambulance, 2)
+else:
+    mean_ambulance = np.nan
+
+# add user to table
+user_data = [path, sex, gamer, mean_all_games, mean_each_game[0], mean_each_game[1], mean_each_game[2],
+             mean_each_game[3], mean_each_game[4], mean_green, mean_red, mean_ambulance]
+
+title = "vysledky.csv"
+if not os.path.isfile(title):
+    with open(title, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Woman", "Gamer", "Mean", "First game", "Second game", "Third game",
+                         "Fourth game", "Fifth game", "On green", "On red", "On ambulance"])
+        writer.writerow(user_data)
+else:
+    with open(title, 'a+', newline='') as write_obj:
+        writer = csv.writer(write_obj)
+        writer.writerow(user_data)
